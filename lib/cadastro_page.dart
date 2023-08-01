66571cbd88a5e6bd6c2af79e52527de8273ce4e2
Lucas Dias <lucas.dias@b2card.com.br>
2023-08-01 15:34:36 -0300

    Validação Senha e Confirmar senha.

--- lib/cadastro_page.dart
+++ lib/cadastro_page.dart
@@ -47,6 +47,27 @@ class _CadastroPageState extends State<CadastroPage> {
     dataNascimentoController.addListener(validateFields);
   }
 
+  String? validateEmail(String? value) {
+    if (value != null && !emailRegex.hasMatch(value)) {
+      return 'Digite um e-mail válido';
+    }
+    return null;
+  }
+
+  String? validateSenha(String? value) {
+    if (value != null && (value.isEmpty || value.length < 6)) {
+      return 'A senha deve ter no mínimo 6 caracteres';
+    }
+    return null;
+  }
+
+  String? validateConfirmarSenha(String? value) {
+    if (value != null && value != senhaController.text) {
+      return 'As senhas não coincidem';
+    }
+    return null;
+  }
+
   void validateFields() {
     final senha = senhaController.text;
     final confirmarSenha = confirmarSenhaController.text;
@@ -56,7 +77,7 @@ class _CadastroPageState extends State<CadastroPage> {
       isButtonEnabled = senha.isNotEmpty &&
           confirmarSenha.isNotEmpty &&
           senha == confirmarSenha &&
-          emailRegex.hasMatch(email) &&
+          (email.isEmpty || emailRegex.hasMatch(email)) &&
           (dataNascimentoController.text.isEmpty ||
               dataNascimentoController.text.trim().isNotEmpty);
     });
@@ -85,123 +106,129 @@ class _CadastroPageState extends State<CadastroPage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Primeiro nome'),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Último nome'),
-                ),
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(labelText: 'E-mail'),
-                ),
-                SizedBox(height: 8.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Nascimento:',
-                      style: labelStyle,
-                    ),
-                    Row(
-                      children: [
-                        DropdownButton<String>(
-                          value: selectedDia,
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              selectedDia = newValue!;
-                              if (dataNascimentoController.text.isNotEmpty) {
-                                validateFields();
-                              }
-                            });
-                          },
-                          items:
-                              dias.map<DropdownMenuItem<String>>((int value) {
-                            return DropdownMenuItem<String>(
-                              value: value.toString().padLeft(2, '0'),
-                              child: Text(
-                                value.toString().padLeft(2, '0'),
-                                style: inputTextStyle,
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        SizedBox(width: 8),
-                        DropdownButton<String>(
-                          value: selectedMes,
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              selectedMes = newValue!;
-                              if (dataNascimentoController.text.isNotEmpty) {
-                                validateFields();
-                              }
-                            });
-                          },
-                          items: meses
-                              .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(
-                                value,
-                                style: inputTextStyle,
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                        SizedBox(width: 8),
-                        DropdownButton<String>(
-                          value: selectedAno,
-                          onChanged: (String? newValue) {
-                            setState(() {
-                              selectedAno = newValue!;
-                              if (dataNascimentoController.text.isNotEmpty) {
-                                validateFields();
-                              }
-                            });
-                          },
-                          items:
-                              anos.map<DropdownMenuItem<String>>((int value) {
-                            return DropdownMenuItem<String>(
-                              value: value.toString(),
-                              child: Text(
-                                value.toString(),
-                                style: inputTextStyle,
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                TextFormField(
-                  decoration:
-                      InputDecoration(labelText: 'Código estabelecimento'),
-                ),
-                TextFormField(
-                  controller: senhaController,
-                  decoration: InputDecoration(labelText: 'Senha'),
-                  obscureText: true,
-                ),
-                TextFormField(
-                  controller: confirmarSenhaController,
-                  decoration: InputDecoration(labelText: 'Confirmar senha'),
-                  obscureText: true,
-                ),
-                SizedBox(height: 32.0),
-                ElevatedButton(
-                  onPressed: isButtonEnabled
-                      ? () {
-                          Navigator.pop(context);
-                        }
-                      : null,
-                  child: Text('Salvar'),
-                ),
-              ],
+          child: Form(
+            autovalidateMode: AutovalidateMode.always,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Primeiro nome'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Último nome'),
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(labelText: 'E-mail'),
+                    validator: validateEmail,
+                  ),
+                  SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Nascimento:',
+                        style: labelStyle,
+                      ),
+                      Row(
+                        children: [
+                          DropdownButton<String>(
+                            value: selectedDia,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedDia = newValue!;
+                                if (dataNascimentoController.text.isNotEmpty) {
+                                  validateFields();
+                                }
+                              });
+                            },
+                            items:
+                                dias.map<DropdownMenuItem<String>>((int value) {
+                              return DropdownMenuItem<String>(
+                                value: value.toString().padLeft(2, '0'),
+                                child: Text(
+                                  value.toString().padLeft(2, '0'),
+                                  style: inputTextStyle,
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                          SizedBox(width: 8),
+                          DropdownButton<String>(
+                            value: selectedMes,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedMes = newValue!;
+                                if (dataNascimentoController.text.isNotEmpty) {
+                                  validateFields();
+                                }
+                              });
+                            },
+                            items: meses
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: inputTextStyle,
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                          SizedBox(width: 8),
+                          DropdownButton<String>(
+                            value: selectedAno,
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                selectedAno = newValue!;
+                                if (dataNascimentoController.text.isNotEmpty) {
+                                  validateFields();
+                                }
+                              });
+                            },
+                            items:
+                                anos.map<DropdownMenuItem<String>>((int value) {
+                              return DropdownMenuItem<String>(
+                                value: value.toString(),
+                                child: Text(
+                                  value.toString(),
+                                  style: inputTextStyle,
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  TextFormField(
+                    decoration:
+                        InputDecoration(labelText: 'Código estabelecimento'),
+                  ),
+                  TextFormField(
+                    controller: senhaController,
+                    decoration: InputDecoration(labelText: 'Senha'),
+                    obscureText: true,
+                    validator: validateSenha,
+                  ),
+                  TextFormField(
+                    controller: confirmarSenhaController,
+                    decoration: InputDecoration(labelText: 'Confirmar senha'),
+                    obscureText: true,
+                    validator: validateConfirmarSenha,
+                  ),
+                  SizedBox(height: 32.0),
+                  ElevatedButton(
+                    onPressed: isButtonEnabled
+                        ? () {
+                            Navigator.pop(context);
+                          }
+                        : null,
+                    child: Text('Salvar'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
