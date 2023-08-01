1a5f2840d5b657ff561af415cfe5dce2a2b351d0
Lucas Dias <lucas.dias@b2card.com.br>
2023-08-01 15:00:49 -0300

    Correção input Data Nascimento.

--- lib/cadastro_page.dart
+++ lib/cadastro_page.dart
@@ -10,6 +10,27 @@ class _CadastroPageState extends State<CadastroPage> {
   TextEditingController confirmarSenhaController = TextEditingController();
   bool isButtonEnabled = false;
 
+  List<int> dias = List.generate(31, (index) => index + 1);
+  List<String> meses = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ];
+  List<int> anos = List.generate(38, (index) => index + 2023);
+
+  String selectedDia = '01';
+  String selectedMes = 'Jan';
+  String selectedAno = '2023';
+
   @override
   void initState() {
     super.initState();
@@ -38,12 +59,19 @@ class _CadastroPageState extends State<CadastroPage> {
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle inputTextStyle = Theme.of(context).textTheme.bodyText1!;
+    final TextStyle labelStyle = inputTextStyle.copyWith(
+      color: Colors.grey,
+      fontSize:
+          inputTextStyle.fontSize! + 2.0, // Aumentando a fonte em 2 pontos
+      fontWeight: FontWeight.normal, // Removendo o negrito
+    );
+
     return Scaffold(
       appBar: AppBar(title: Text('Cadastro')),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: 32.0), // Espaçamento horizontal de 2 cm
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -57,8 +85,75 @@ class _CadastroPageState extends State<CadastroPage> {
                 TextFormField(
                   decoration: InputDecoration(labelText: 'E-mail'),
                 ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Data de nascimento'),
+                SizedBox(height: 8.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Nascimento:',
+                      style: labelStyle, // Utilizando o novo estilo do label
+                    ),
+                    Row(
+                      children: [
+                        DropdownButton<String>(
+                          value: selectedDia,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedDia = newValue!;
+                            });
+                          },
+                          items:
+                              dias.map<DropdownMenuItem<String>>((int value) {
+                            return DropdownMenuItem<String>(
+                              value: value.toString().padLeft(2, '0'),
+                              child: Text(
+                                value.toString().padLeft(2, '0'),
+                                style: inputTextStyle,
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                        SizedBox(width: 8),
+                        DropdownButton<String>(
+                          value: selectedMes,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedMes = newValue!;
+                            });
+                          },
+                          items: meses
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: inputTextStyle,
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                        SizedBox(width: 8),
+                        DropdownButton<String>(
+                          value: selectedAno,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedAno = newValue!;
+                            });
+                          },
+                          items:
+                              anos.map<DropdownMenuItem<String>>((int value) {
+                            return DropdownMenuItem<String>(
+                              value: value.toString(),
+                              child: Text(
+                                value.toString(),
+                                style: inputTextStyle,
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
                 TextFormField(
                   decoration:
@@ -66,22 +161,19 @@ class _CadastroPageState extends State<CadastroPage> {
                 ),
                 TextFormField(
                   controller: senhaController,
-                  decoration:
-                      InputDecoration(labelText: 'Senha'), // Campo de senha
-                  obscureText: true, // Esconde o texto digitado
+                  decoration: InputDecoration(labelText: 'Senha'),
+                  obscureText: true,
                 ),
                 TextFormField(
                   controller: confirmarSenhaController,
-                  decoration: InputDecoration(
-                      labelText: 'Confirmar senha'), // Campo de confirmar senha
-                  obscureText: true, // Esconde o texto digitado
+                  decoration: InputDecoration(labelText: 'Confirmar senha'),
+                  obscureText: true,
                 ),
-                SizedBox(height: 32.0), // Espaçamento vertical de 2 cm
+                SizedBox(height: 32.0),
                 ElevatedButton(
                   onPressed: isButtonEnabled
                       ? () {
-                          Navigator.pop(
-                              context); // Volta para a página de login
+                          Navigator.pop(context);
                         }
                       : null,
                   child: Text('Salvar'),
